cc6db62fbcb306db30700e270e3be028bc518960
wwongpai  <warach.wongpairoj@datadoghq.com>
2022-11-19 16:18:15 +0700

    first commit and all

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const bunyan = require('bunyan');
+const log = bunyan.createLogger({
+	name: "myapp",
+});
+
+const express = require('express')
+const app = express()
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.get('/hello', (req, res) => {
+  log.info('Hello was accessed.');
+  res.send('Hello World!')
+})
+
+app.get('/hi', (req, res) => {
+  log.info('Hi was accessed.');
+  res.send('Hi World!')
+})
+
+app.get('/error', (req, res) => {
+	log.error('Some fake error here.');
+	res.send('Error!')
+  })
+
+app.get('/warn', (req, res) => {
+	log.warn('Some fake warning here.');
+	res.send('Warning!')
+  })
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
